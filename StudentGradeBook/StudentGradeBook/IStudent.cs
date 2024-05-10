24fc640c1d69fabf234a878dcae8761aa6a5d2a9
maciejleszcz95 <maciejleszczynski@icloud.com>
2024-05-10 13:19:27 +0200

    Student interface has been created

---
+++ StudentGradeBook/StudentGradeBook/IStudent.cs
@@ -0,0 +1,23 @@
+﻿using static StudentGradeBook.StudentBase;
+
+namespace StudentGradeBook
+{
+    public interface IStudent
+    {
+        string Name { get; }
+        string Surname { get; }
+        char Gender { get; }
+        int Age { get; }
+        string GenderInString { get; }
+
+        void AddGrade(float grade);
+        void AddGrade(int grade);
+        void AddGrade(string grade);
+        void AddGrade(char grade);
+        
+        event GradeAddedDelegate GradeAdded;
+        
+        List<string> GetGrades();
+        Statistics GetStatistics();
+    }
+}
